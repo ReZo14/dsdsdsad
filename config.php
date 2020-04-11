f2b2daee97649937d3805dab9a123413f79678fc
ReZo14 <53716874+ReZo14@users.noreply.github.com>
2020-04-12 00:56:13 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+"title"=>"TMail - Multi Domain Temporary Email System",
+"host"=>"",
+"user"=>"",
+"pass"=>"",
+"domains"=>array(),
+"forbidemail"=>array("admin","webmaster","idiot"),
+"admin"=>"tmail123",
+);
+?>
---
+++ options.php
@@ -0,0 +1,15 @@
+<?php
+$option = array(
+"deleteDays"=>"3",
+"refreshRate"=>"5",
+"ads"=>"",
+"ssl"=>"yes",
+"logs"=>"no",
+"pushNotifications"=>"yes",
+"timezone"=>"Asia/Kolkata",
+"aboutus"=>"<h1>About TMail</h1><br><br><p>TMail is disposable email system which can be hosted on your own standard PHP Web Hosting. All you need is few common PHP extensions and IMAP services. The system is as simple as possible, with minimal codebase and complexity.</p>",
+"defaultlanguage"=>"en",
+"linksTitle"=>array("Home","Purchase","Developer"),
+"linksValue"=>array("https://envato.stackants.com/tmail/demo/classic/","https://codecanyon.net/item/tmail-multi-domain-temporary-email-system/20177819","https://codecanyon.net/user/harshitpeer"),
+);
+?>
